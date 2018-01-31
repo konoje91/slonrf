bb7f7eba2227c9e3f40cbaa92170bac2756436c3
Batters91 <ambrela.dj@mail.ru>
2018-01-31 08:41:36 +0400

    add links actions and block offers

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    //
+
+	$('a.about-company-link').on('click', function(){
+				$('html, body').animate({
+                    scrollTop: $(".about-company").offset().top
+                }, 1000);
+	});
+
+	$('a.trust-us-link').on('click', function(){
+				$('html, body').animate({
+                    scrollTop: $(".trust-us").offset().top
+                }, 1000);
+	});
+
+	$('a.offers-link').on('click', function(){
+				$('html, body').animate({
+                    scrollTop: $(".offers").offset().top
+                }, 1000);
+	});
+
+	$('a.contacts-link').on('click', function(){
+				$('html, body').animate({
+                    scrollTop: $(".contacts").offset().top
+                }, 1000);
+	});
+
 });
